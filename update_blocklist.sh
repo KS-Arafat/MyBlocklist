b5bbaac62354430f282602607bf99d6a3dcbd5ef
KS-Arafat <sfnarafat@gmail.com>
2026-01-24 14:20:47 +0600

    Sort By SDL

--- update_blocklist.sh
+++ update_blocklist.sh
@@ -14,6 +14,20 @@ touch "$OUTPUT"
 sed 's/^[[:space:]]*//;s/[[:space:]]*$//' "$INPUT" | \
 grep -v '^$' >> "$OUTPUT"
 
-# Sort and deduplicate
-sort -u "$OUTPUT" > "$TMP"
+# Sort and deduplicate by subdomain
+# sort -u "$OUTPUT" > "$TMP"
+# mv "$TMP" "$OUTPUT"
+
+# Sort and deduplicate by domain
+awk -F. '{
+    n=NF
+    # capture the last 2 fields as SDL
+    if(n>=2){
+        sdl=$(n-1)"."$n
+        print sdl,$0
+    } else {
+        print $0,$0
+    }
+}' "$OUTPUT" | sort -k1,1 -u | awk '{print $2}' > "$TMP"
 mv "$TMP" "$OUTPUT"
+rm -f "$TMP"
